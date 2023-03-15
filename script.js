feba0e956bbfad562bbcab3b8e892b4e52af005e
SergeyNitsenko <s.erg-86@mail.ru>
2023-03-15 16:04:37 +0300

    add: added JS_HW4

---
+++ script.js
@@ -0,0 +1,171 @@
+"use strict"
+
+
+//Task 1
+
+{
+    // let m = [1, 2, 3, 4, 5];
+    // console.log(m);
+
+    // for (i = 0; i < m.length; i++) {
+    //     console.log(m[i]);
+    // }
+}
+
+
+//Task 2
+
+{
+    // let num = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+
+    // let i;
+
+    // for (i = 0; i < num.length; i++) {
+    //     if (num[i] > -10 && num[i] < -3) {
+    //         console.log(num[i]);
+    //     }
+    // }
+}
+
+
+//Task 3 
+
+{
+    // let nums = [];
+    // let result = 0;
+
+
+    // for (let i = 23; i <= 57; i++) {
+    //     nums.push(i);
+    //     result += i;
+    // }
+    // console.log(nums);
+    // console.log('Сумма = '+ result);
+
+
+
+    //let j = 23;
+    // while (j <= 57) {
+    //     nums.push(j)
+    //     j++;
+    // }
+    // console.log(nums);
+
+}
+
+
+//Task 4
+
+{
+    // let arr = ['10', '20', '30', '50', '235', '3000'];
+
+    // for (let i of arr) {
+    //     if (+i[0] === 1 || +i[0] === 2 || +i[0] === 5) {
+    //         console.log(i)
+    //     }
+    // }
+}
+
+
+// Task 5
+
+{
+    // let week = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+    // for (let day = 0; day < week.length; day++) {
+    //     if (week[day] !== 'СБ' && week[day] !== 'ВС') {
+    //         document.write(week[day] + ' ');
+    //     } else {
+    //         document.write(week[day].bold() + ' ')
+    //     }
+    // }
+
+}
+
+
+
+//Task 6
+
+{
+    // let arr = [1, 2, 3,];
+
+    // arr[arr.length] = '4';
+    // console.log(arr);
+}
+
+
+//Task 7
+
+{
+    // let vers = [];
+
+    // while (true) {
+    //     let sum = prompt('Введите любое число: ');
+
+    //     if (sum === '') {
+    //         break;
+    //     }
+    //     vers.push(+sum);
+    // }
+
+    // //vers.push(sum);
+    // console.log(vers);
+    // console.log(vers.sort())
+}
+
+
+//Task 8
+
+{
+    // let arr = [12, false, 'Текст', 4, 2, -5, 0];
+    // console.log(arr);
+
+    // while (1 > 0) {
+    //     console.log(arr.reverse());
+    //     break;
+    // }
+}
+
+
+//Task 9
+
+{
+    // let arr = [5, 9, 21, , , 9, 78, , , , 6];
+    // let sum = 0;
+    
+    // console.log(arr)
+
+
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === undefined) {
+    //         sum += 1;
+    //     } 
+    //     //
+    // }
+    // console.log(sum)
+
+}
+
+
+//Task 10 
+
+{
+    // let arr = [48, 9, 0, 4, 21, 2, 1, 0, 8, 84, 76, 8, 4, 13, 2];
+    // let sum = 0;
+    // let indexOf = arr.indexOf(0);
+    // let lastIndexOf = arr.lastIndexOf(0);
+    // let i ;
+    // // console.log(arr.length);
+    // // console.log(arr)
+
+    // for (i = indexOf; i < lastIndexOf; i++) {
+    //     sum += arr[i];
+    // }
+    // console.log(sum);
+}
+
+
+
+
+
